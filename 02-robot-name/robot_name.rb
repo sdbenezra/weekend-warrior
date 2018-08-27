4ea5513c563dccb9c38e661532f17abf2e0941b1
Sigrid Benezra <sdbenezra@gmail.com>
2018-08-26 19:56:38 -0700

    Complete 02-robot-name.rb

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,12 @@
+require 'pry'
+class Robot
+  attr_accessor :name
 
+  def initialize
+    @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+  end
+
+  def reset
+    @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+  end
+end
