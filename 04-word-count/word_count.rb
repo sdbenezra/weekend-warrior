9e9a92b3dbe4626b77573b7bed614e385543bed8
Sigrid Benezra <sdbenezra@gmail.com>
2018-09-16 13:14:01 -0700

    Complete word_count.rb

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,12 @@
-
+require 'pry'
+def words(string)
+  word_count = {}
+  string.split.each do |word|
+    if word_count.keys.include?(word)
+      word_count[word] += 1
+    else
+      word_count[word] = 1
+    end
+  end
+  return word_count
+end
